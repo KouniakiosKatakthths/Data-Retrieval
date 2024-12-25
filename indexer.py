9dd6079b94d8062dd036d14f78d358fcb3aa9912
Theofanis kouniakis <62156040+KouniakiosKatakthths@users.noreply.github.com>
2024-12-25 03:07:47 +0200

    first commit

---
+++ indexer.py
@@ -0,0 +1,42 @@
+import json
+from collections import defaultdict
+
+def create_inverted_index(data):
+    inverted_index = defaultdict(list)
+
+    # For all the enties in the file
+    for index, site_data in enumerate(data):
+        link = site_data["website_url"]
+
+        # For each paragraph of the entry
+        for p_index, paragraph in enumerate(site_data["content"]):
+            # Take only the unique words inside the paragraph
+            # Split them using the ' '
+            words = set(paragraph.split()) 
+            
+            # For every unique word in paragraph
+            for word in words:
+                # If the website link is not present in the on the word entry append it  
+                if link not in inverted_index[word]:
+                    inverted_index[word].append(link)
+
+    return inverted_index
+
+# Open the save file
+filepath = "wiki_scrape.json"
+with open(filepath, "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+inverted_index = create_inverted_index(data)
+
+# Convert to dict
+inverted_index = dict(inverted_index)
+
+# Save to JSON
+index_filename = "inverted_index.json"
+try:
+    with open(index_filename, "w", encoding="utf-8") as file:
+        json.dump(inverted_index, file, ensure_ascii=False, indent=4)
+    print(f"Inverted index saved to: {index_filename}")
+except IOError as e:
+    print(f"Error saving inverted index: {e}")
---
+++ preprocessor.py
@@ -0,0 +1,82 @@
+import json
+import re       #REGEX
+
+import nltk.stem
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+def clear_special_char(text: str) -> str:
+    regex = r"[^a-zA-Z0-9\s]"  # Keep only letters and numbers
+    text = re.sub(regex, "", text)
+    text = text.strip()
+    return text
+
+def preprocess_text(text: str) -> str:
+    # Lemmatizer and stop word objects for english
+    lemmatizer = WordNetLemmatizer()
+    stop_words = set(stopwords.words("english"))
+
+    # Tokenize the paragraph
+    tokens = word_tokenize(text)
+
+    # Remove all the words inside the stop word container
+    non_stopwords_tokens = [word for word in tokens if word.lower() not in stop_words]
+
+    # Lematize the remain words
+    lemmatized = [lemmatizer.lemmatize(word) for word in non_stopwords_tokens]
+
+    # Join all the lemmatized words into a string
+    final_text = " ".join(lemmatized)
+    return final_text
+
+# Open the save file
+filepath = "wiki_scrape.json"
+with open(filepath, "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+# Parse all the text in the scrape file
+parsed_data = {}
+for index, site_data in enumerate(data):
+    parsed_content = []
+    link = site_data["website_url"]
+    for p_index, paragraph in enumerate(site_data["content"]):
+        # For each paragraph run clean and preprocess
+        clear_paragraph = clear_special_char(paragraph)
+        parsed_paragraph = preprocess_text(clear_paragraph)
+        parsed_content.append(parsed_paragraph)
+    
+    parsed_data[link] = parsed_content
+
+# Convert to json object
+json_object = [
+    {
+        "website_url": website,
+        "content": data_list, 
+    }
+    for website, data_list in parsed_data.items()
+]
+
+# Save as JSON file
+filename = "parsed_scrape.json"
+try:
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(json_object, file, indent=4)
+    print(f"Data saved to JSON file: {filename}")
+except IOError as e:
+    print(f"Error saving to JSON file: {e}")
+
+# Download nltk dependencies (If needed)
+#nltk.download()
+#nltk.download("punkt")
+#nltk.download("stopwords")
+#nltk.download("wordnet")
+
+#import nltk
+#try:
+#    nltk.data.find('corpora/stopwords')
+#    nltk.data.find('corpora/wordnet')
+#except LookupError:
+#    nltk.download('stopwords')
+#    nltk.download('wordnet')
+#    nltk.download('punkt')

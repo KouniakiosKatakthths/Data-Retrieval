d3d27f09ee7a65dfb8ff73353dfbd9ec734f121b
Theofanis kouniakis <62156040+KouniakiosKatakthths@users.noreply.github.com>
2024-12-26 18:11:13 +0200

    tyyt

--- search_engine.py
+++ search_engine.py
@@ -1,3 +1,6 @@
+import json
+
+# NLTK imports
 import nltk.stem
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
@@ -15,23 +18,38 @@ def request_query(query: str, index: dict) -> set:
     non_stopwords_tokens = [word for word in tokens if word.lower() not in stop_words]
 
     # Lematize the remain words
-    lemmatized = [lemmatizer.lemmatize(word) for word in non_stopwords_tokens]
+    lemmatized_query = [lemmatizer.lemmatize(word) for word in non_stopwords_tokens]
 
     result = set()
     op = "or"
 
     for token in lemmatized_query:
+        # Chnage mode
         if token.lower() in { "and", "or", "not" }:
             op = token.lower()
-    
+            continue
+
+        # Token is not found
+        if token not in index:
+            continue
+
+        url_list = index[token]
+        
 
     return result
 
-print("\n=== Avaiable Retrieval Algorithms ===")
-print("1 -> Boolean Retrieval")
-print("2 -> Vector Space Model (VSM)")
-print("3 -> Probabilisic Retrieval Models (Okapi BM25)")
-algorithm = input("Select retrieval algorithn (1/2/3): ")
-while (algorithm != '1' and algorithm != '2' and algorithm != '3'):
-    algorithm = input("Invalid option. Select retrieval algorithn (1/2/3): ")
+#print("\n=== Avaiable Retrieval Algorithms ===")
+#print("1 -> Boolean Retrieval")
+#print("2 -> Vector Space Model (VSM)")
+#print("3 -> Probabilisic Retrieval Models (Okapi BM25)")
+#algorithm = input("Select retrieval algorithn (1/2/3): ")
+#while (algorithm != '1' and algorithm != '2' and algorithm != '3'):
+#    algorithm = input("Invalid option. Select retrieval algorithn (1/2/3): ")
 
+# Open the save file
+filepath = "inverted_index.json"
+with open(filepath, "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+query = input("Request query: ")
+request_query(query, data)

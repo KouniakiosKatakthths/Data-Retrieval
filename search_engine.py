70eaf7a7a7e195dd26c81098ea9a99f384ea7430
sotsouk03 <sotirissouk@gmail.com>
2024-12-26 19:09:01 +0200

    Squashed commit of the following:
    commit 764a77bc863db12c6c5689733a322f99b427c47b
    Author: Theofanis kouniakis <62156040+KouniakiosKatakthths@users.noreply.github.com>
    Date:   Thu Dec 26 18:38:44 2024 +0200
    
        Finished search engine pt1 fixed bug in the indexer
    
    commit d3d27f09ee7a65dfb8ff73353dfbd9ec734f121b
    Author: Theofanis kouniakis <62156040+KouniakiosKatakthths@users.noreply.github.com>
    Date:   Thu Dec 26 18:11:13 2024 +0200
    
        tyyt

--- search_engine.py
+++ search_engine.py
@@ -1,3 +1,6 @@
+import json
+
+# NLTK imports
 import nltk.stem
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
@@ -6,11 +9,13 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-"""
+
 def request_query(query: str, index: dict) -> set:
+    logic_operators = {"and", "or", "not"};
+
     # Init nltk objects
     lemmatizer = WordNetLemmatizer()
-    stop_words = set(stopwords.words('english'))
+    stop_words = set(stopwords.words('english')) - logic_operators
 
     # Tokenize query
     tokens = word_tokenize(query.lower())
@@ -19,27 +24,47 @@ def request_query(query: str, index: dict) -> set:
     non_stopwords_tokens = [word for word in tokens if word.lower() not in stop_words]
 
     # Lematize the remain words
-    lemmatized = [lemmatizer.lemmatize(word) for word in non_stopwords_tokens]
+    lemmatized_query = [lemmatizer.lemmatize(word) for word in non_stopwords_tokens];
 
+    # Remove any duplicates
     result = set()
+
+    # Default search op is logic or
     op = "or"
 
     for token in lemmatized_query:
-        if token.lower() in { "and", "or", "not" }:
+        # Chnage mode
+        if token.lower() in logic_operators:
             op = token.lower()
-    
+            continue
+
+        # Token is not found
+        if token not in index:
+            continue
+        
+        url_list = set(index[token])
+
+        # Sets allow logic operations on 
+        if op == "or":
+            result |= url_list  # If or join the two url lists
+        elif op == "and":
+            result &= url_list  # If and take the common links only
+        elif op == "not":
+            result -= url_list  # If not remove the links from the result
 
     return result
 
-print("\n=== Avaiable Retrieval Algorithms ===")
-print("1 -> Boolean Retrieval")
-print("2 -> Vector Space Model (VSM)")
-print("3 -> Probabilisic Retrieval Models (Okapi BM25)")
-algorithm = input("Select retrieval algorithn (1/2/3): ")
-while (algorithm != '1' and algorithm != '2' and algorithm != '3'):
-    algorithm = input("Invalid option. Select retrieval algorithn (1/2/3): ")
+# Open the save file
+filepath = "inverted_index.json"
+with open(filepath, "r", encoding="utf-8") as file:
+    data = json.load(file)
 
-"""
+
+query = input("Request query: ")
+sites = request_query(query, data)
+
+for res in sites:
+    print(res) 
 
 #β) Κατάταξη αποτελεσμάτων (Ranking)
 
@@ -57,4 +82,3 @@ def ranking(method_id,documents,query):
         return np.argsort(scores[0])[::-1]
 
     return
-    
